b985c7763b4c399216bd831aa442fecf76b49906
hashworks <mail@hashworks.net>
2018-09-15 17:56:03 +0200

    Drop various cipher suites

--- server/security.go
+++ server/security.go
@@ -23,14 +23,8 @@ func (s Server) RunTLS(address string) error {
 		TLSConfig: &tls.Config{
 			MinVersion: tls.VersionTLS12,
 			CipherSuites: []uint16{
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA, // Old Safaris (<= Safari 8 / OS X 10.10)
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			},
 			GetCertificate: m.GetCertificate,
 			NextProtos: []string{
