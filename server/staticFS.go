dd8cfd1468ed41eff8fc6e068b8ebe95d7f39c83
hashworks <mail@hashworks.net>
2018-10-20 12:00:31 +0200

    Remove dev println in prefix HTTPFS
    Signed-off-by: hashworks <mail@hashworks.net>

--- server/staticFS.go
+++ server/staticFS.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"github.com/hashworks/hashworksNET/server/bindata"
 	"net/http"
 	"os"
@@ -12,7 +11,6 @@ type prefixHTTPFS struct {
 }
 
 func (phfs *prefixHTTPFS) Open(path string) (http.File, error) {
-	fmt.Println(phfs.prefix + path)
 	f, err := bindata.FS.OpenFile(bindata.CTX, phfs.prefix+path, os.O_RDONLY, 0644)
 	if err != nil {
 		return nil, err
